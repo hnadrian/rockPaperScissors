73381a42e5a1a7696199346260ebe73e049c8216
hnadrian <hngt.adrian@gmail.com>
2021-04-05 02:38:49 -0700

    Move JS code to seperate file

---
+++ game.js
@@ -0,0 +1,33 @@
+function computerPlay() {
+    var moves = ["Rock", "Paper", "Scissors"];
+    return moves[Math.floor(Math.random() * moves.length)];
+  }
+
+  function singleRoundResult(computerSelection, playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    let result = "No result.";
+    if (computerSelection === playerSelection) {
+      result = "Tie with" + " " + computerSelection + "."
+    } else if (computerSelection === "paper") {
+      if (playerSelection === "scissors") result = "You win! Scissors beat Paper.";
+      else if (playerSelection === "rock") result = "You lose! Paper beats Rock.";
+    } else if (computerSelection === "rock") {
+      if (playerSelection === "paper") result = "You win! Paper beats Rock.";
+      else if (playerSelection === "scissors") result = "You lose! Rock beats Scissors.";
+    } else {
+      if (playerSelection === "paper") result = "You lose! Scissors beat Paper.";
+      else if (playerSelection === "rock") result = "You win! Rock beat Scissors.";
+    }
+    return result;
+  }
+
+  function game() {
+    console.log("ROCK PAPER SCISSORS GAME");
+    for (var i = 0; i < 5; i ++) {
+      let playerSelection = promt("Enter your move (Rock, Paper or Scissors): ");
+      let computerSelection = computerPlay();
+      console.log(singleRoundResult(computerSelection, playerSelection));
+    }
+  }
+  game();
